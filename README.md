e6f76ac1e2a7b2e3a91ab39244cd32c9923a98cb
gabrielgudino <gmgudino.ara@gmail.com>
2023-07-04 12:25:33 -0300

    el main cambia de nombre a main_prueba y comenzamos a trabajar sobre el proyecto principal

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# creando_una_API
+# Tools API
+
+En este proyecto se quiere hacer un sistema de recomendación de herramientas informáticas según sea el perfil del usuario.
+Para ello, se debe poblar una BB.DD. con herramientas informáticas de toda índole (software, data, cloud, etc.).
+De esta manera, se harán operaciones de lectura, creación, actualización y borrado en la BB.DD. Para ello, se solicita la 
+implementación de un API que pueda hacer dichas operaciones.
+
+Para poder cumplimentar este proyecto deberemos hacer un CRUD mediante un API. Para ésto, utilizaremos FastAPI mediante sus
+métodos GET, POST, PUT y DELETE. Un vez terminado se desplegó en Deta Space.
--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-### AQUI ES DONDE VAMOS A DESARROLLAR EL CODIGO NECESARIO PARA PODER LEVANTAR UN SERVIDOR WEB API Y CREAR NUESTRO ENDPOINT
-
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI() # Lo instanciamos. Esta variable nos va a permitir correr un proyecto
+# Como vamos a trabajar con modelos de datos usando Pydantic y estamos usando un servicio web que nos
+# retorna un archivo JSON formateado, necesitamos algunos utilitarios de FastAPI para poder transformar
+# las instancias de las clases con Pydantic a un diccionario JSON y poder retornarlos sin problemas. Para 
+# esto vamos a importa las siguientes librerias:
 
-@app.get(path="/") # Se llama "path operation decorator". Es un decorador para una función a crear. Utiliza el objeto get que viene del obj app.
-def index():
-    return {"message":"Mira Shaila!!!!! MI PRIMER API!!!!"}
+from fastapi.encoders import jsonable_encoder # transforma la instancia que tenemos con el modelo de datos a un diccionario JSON
+from fastapi.responses import JSONResponse # formatea la respuesta JSON y poder incluir el código de estado en la API
 
-# Lo que acabamos de hacer es que cada vez que se consulte a la ruta "/"(o la ruta por defecto) se va a
-# ejecutar la funcion index
+from pydantic import BaseModel # BaseModel nos servirá para poder crear las Clases/modelos de datos. 
+# from typing import Optional # lo utilizamos si en el modelo de datos vamos a tener variables opcionales.
 
-# luego desde la terminal iniciaremos la API. Esto lo haremos con UVICORN.
-# Ejecutaremos "uvicorn main:app --reload" ------> "main" por el nombre del archivo. "app" que es el objeto de nuestra aplicación,
-# "--reload" es un modificador que nos permitira realizar cambios en el codigo sin tener que levantar el servidor nuevamente, sino
-# que lo detecta y lo vuelve a correr automáticamente.
 
-# EN BASE A ESTO UVICORN VA A ESTAR CORRIENDO POR DEFECTO EN EL LOCALHOST/PUERTO 8000
