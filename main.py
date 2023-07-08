80d866b7541281ead7caf8755ff87b07c5ff9e15
gabrielgudino <gmgudino.ara@gmail.com>
2023-07-08 15:54:19 -0300

    agregamos un endpoint con el método PUT

--- main.py
+++ main.py
@@ -86,3 +86,29 @@ async def create_tool(tool:Tool): # Lo q hace FastAPI es leer el "request body",
     json_data = jsonable_encoder(tool)
     return JSONResponse(content=json_data, status_code=201)
 
+# 9no commit - "Método PUT"
+# Este método lo utilizaremos para reemplazar o actualizar un registro en nuestra API. Para ello tenemos que recibir 2 párametros, el id a buscar a efectos de
+# que si lo encuentra actualiza el "Documento". También tenemos que recibir el objeto que queremos reemplazar de esta base de datos. De esta manera crearemos
+# un endpoint que reciba un "path parameter" para el "id" y un "request body" para el objeto que queremos actualizar.
+# LA FUNCIÓN find_index_by_id() DEBIERA ESTAR EN LA CABEZA DEL SCRIPT POR CUESTIONES DE PROLIJIDAD, PERO LA DEJAREMOS AQUI POR CUESTIONES DIDÁCTICAS.
+def find_index_by_id(tool_id): # el argumento es el "id" de la herramienta a buscar.
+    _index = None # por defecto tiene un valor nulo que retornará si no encuentra el "id".
+    for index, value in enumerate(tools_list): #enumerate() nos convierte la lista en constants.py en un objeto JSON.
+        if value['id'] == tool_id:
+            _index = index # guarda como clave el índice 
+            break
+    return _index
+
+# Este endpoint es invocado por el método http "put", el cual va a recibir 2 parámetros. El primero será un "path parameter" el cual guardará el "id" de la 
+# herramienta a actualizar (si esta existe la actualizamos, sino retornamos un código 404).  El segundo será un "request body" el objeto que queremos reemplazar.
+# Para poder hacer todo ésto necesitaremos encontrar el índice de la lista del "id" que queremos actualizar. Es por ésto que se creo la función find_index_by_id().
+@app.put(path='/api/tools/{tool_id}') # Método PUT con su ruta y su parámetro de path
+async def update_tool(tool_id: str, tool: Tool): # La función asincrónica se encargará de actualizar el "id". Recibe 2 parámetros, el "id" y el objeto Tool como "request body".
+    if tool.id is None: # aquí aplicamos una lógica similar al método POST. Validamos que el tool_id sea nulo para poder generarlo.
+        tool.id = f'{tool.name}-{tool.category}' # Al ser nulo generamos el "id" concatenando el nombre y la categoría en tool.
+    index_update = find_index_by_id(tool_id=tool_id) # Una vez listo el elemento para actualizar buscamos el elemento en la lista guardandolo en "index_update"
+    if index_update is None: # si la busqueda de indice es nula tenemos que retornar None y un 404. 
+        return JSONResponse(content=None, status_code=404)
+    else: # aca si ha sido encontrado el index. Entonces reemplazamos el elemento.
+        tools_list[index_update] = tool.dict() # lo actualizamos con el objeto tool que indicamos como argumento en update_tool()
+    return JSONResponse(content=True, status_code=200)
